bce1719ef8ff6c130e1dac9a35e7970e70312dee
lyzerk <alm.ozdmr@gmail.com>
2020-11-15 21:22:04 +0300

    color change

--- src/PromptConfig.cs
+++ src/PromptConfig.cs
@@ -5,7 +5,7 @@ namespace PromptCLI
     public class PromptConfig
     {
         public static PromptConfig Default { get; } = new PromptConfig();
-        public ConsoleColor QuestionColor { get; } = ConsoleColor.Magenta;
+        public ConsoleColor QuestionColor { get; } = ConsoleColor.Gray;
         public ConsoleColor AnswerColor { get; } = ConsoleColor.Cyan;
         public ConsoleColor CursorColor { get; } = ConsoleColor.Red;
         public string Cursor { get; } = "> ";
